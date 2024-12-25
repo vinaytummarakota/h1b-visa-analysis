235a38ac51006767051835da9a0156b2880e1634
sst7 <sst7@rice.edu>
2024-12-25 18:58:29 -0500

    Remove h1b1 analysis and add analysis of missing data + FY2025 data

--- helper_functions/clean_data.R
+++ helper_functions/clean_data.R
@@ -2,9 +2,10 @@ library(docstring)
 library(readxl)
 library(janitor)
 library(tidyverse)
+library(stringr)
 
 clean_h1b_df <- function(path, winning_statuses) {
-  #' @description: Cleans the raw H1-B spreadsheet data and saves the result in the {clean_data} folder
+  #' @description: Cleans the raw H1-B lottery registration data and saves the result in the {clean_data} folder
   #' 
   #' @param path: A string representing the path to the dataframe that needs to be cleaned
   
@@ -13,17 +14,37 @@ clean_h1b_df <- function(path, winning_statuses) {
   cleaned_df <- df %>%
     clean_names() %>%
     select(country_of_nationality, ben_year_of_birth, city, state, employer_name, lottery_year, status_type, 
-           ben_multi_reg_ind, ben_education_code, naics_code) %>%
+           ben_multi_reg_ind, ben_education_code, rec_date, naics_code) %>%
     mutate(reg_id = row_number(), 
            age_in_years = as.integer(lottery_year) - as.integer(ben_year_of_birth),
            city = paste(city, state), 
            lottery_year = as.integer(lottery_year), 
            won_lottery = ifelse(is.na(status_type), NA, status_type == "SELECTED"), 
            had_multiple_regs = as.integer(ben_multi_reg_ind) == 1, 
-           naics_code = ifelse(naics_code == "", NA, substr(naics_code, 1, 2)), 
-           has_grad_degree = ifelse(ben_education_code == "", NA, ben_education_code %in% c("G", "H", "I"))) %>%
+           submitted_petition = ifelse(rec_date == "" | is.na(rec_date), FALSE, TRUE), 
+           naics_code = ifelse(naics_code == "" | is.na(naics_code), NA, substr(naics_code, 1, 2)), 
+           degree_level_is_missing = ben_education_code %in% c("", "J", "K", "R", NA), 
+           has_grad_degree = ifelse(ben_education_code %in% c("", "J", "K", "R", NA), NA, ben_education_code %in% c("G", "H", "I"))) %>%
     rename(country_code = country_of_nationality) %>%
     select(-c(ben_year_of_birth, state, status_type, ben_education_code))
+  
+  return(cleaned_df)
+}
+
+clean_volume_df <- function(path, winning_statuses) {
+  #' @description: Cleans the raw H1-B application volume data and saves the result in the {clean_data} folder
+  #' 
+  #' @param path: A string representing the path to the dataframe that needs to be cleaned
+   
+  df <- read.csv(path)
+  
+  colnames(df) <- c("lottery_year", "total_registrations", "eligible_registrations", "eligible_single_registrations", "eligible_multiple_registrations", "selected_registrations")
+  
+  cleaned_df <- df %>%
+    select(c("lottery_year", "eligible_single_registrations", "eligible_multiple_registrations")) %>%
+    pivot_longer(!lottery_year, names_to = "registration_type", values_to = "num_registrations") %>%
+    mutate(num_registrations = as.integer(str_replace_all(num_registrations, "[*,]", "")), 
+           registration_type := recode(registration_type, eligible_single_registrations = "Single Registrations", eligible_multiple_registrations = "Multiple Registrations"))
   
   return(cleaned_df)
 }
--- helper_functions/exploratory_analysis.R
+++ helper_functions/exploratory_analysis.R
@@ -1,10 +1,52 @@
 library(docstring)
 library(tidyverse)
 library(glue)
+library(scales)
+
+identify_top_n_categories <- function(df, segment_col, segment_name, num_categories, label_mapping) {
+  #' @description: Identifies the top-N values of {segment_col} based on applicant volume
+  #' 
+  #' @param df: The dataframe containing the H-1B results
+  #' @param segment_col: A string containing the name of the column along which outcomes should be calculated 
+  #' @param segment_name: A string containing the plain language name of the column along which outcomes should be calculated
+  #' @param num_categories: An integer representing the # of categories we wish to include in the resulting dataframe (based on the N most frequent categories in the dataset)
+  #' @param label_mapping: A list mapping the values in a given column to more readable values; also maps
+  #' name changes to the same group (for the highest volume groups only)
+  
+  top_n_categories <- df %>%
+    mutate(!!as.symbol(segment_col) := recode(!!as.symbol(segment_col), !!!label_mapping)) %>%
+    group_by(!!as.symbol(segment_col)) %>%
+    summarise(num_obs = n()) %>%
+    arrange(desc(num_obs)) %>%
+    top_n(num_categories) %>%
+    pull(!!as.symbol(segment_col))
+  
+  return(top_n_categories)
+}
+
+estimate_outcome_rates <- function(df, segment_col, segment_name, outcome_col, outcome_name, top_n_categories, label_mapping) {
+  #' @description: Estimates the rate of {outcome_col} by {segment_col} across lottery years
+  #'  
+  #' @param df: The dataframe containing the H-1B results
+  #' @param segment_col: A string containing the name of the column along which outcomes should be calculated 
+  #' @param segment_name: A string containing the plain language name of the column along which outcomes should be calculated
+  #' @param outcome_col: A string containing the name of the column containing the outcome of interest
+  #' @param outcome_name: A string containing the plain language name of the column containing the outcome of interest
+  #' @param top_n_categories: The top-N values of {segment_col} by applicant volume
+  #' @param label_mapping: A list mapping the values in a given column to more readable values; also maps
+  #' name changes to the same group (for the highest volume groups only)
+  
+  outcome_df <- df %>%
+    mutate(!!as.symbol(segment_col) := recode(!!as.symbol(segment_col), !!!label_mapping)) %>%
+    filter(!!as.symbol(segment_col) %in% top_n_categories) %>%
+    group_by(lottery_year, !!as.symbol(segment_col)) %>%
+    summarise(outcome_rate = sum(!!as.symbol(outcome_col))/n())
+  
+  return(outcome_df)
+}
 
 bump_plot <- function(df, segment_col, segment_name, outcome_col, outcome_name, num_categories, label_mapping, title) {
-  #' @description: Estimates the rate of {outcome_col} by {segment_col} across lottery years and 
-  #' plots the results using a bump plot
+  #' @description: Estimates the rate of {outcome_col} by {segment_col} across lottery years and plots the results using a bump plot
   #' 
   #' @param df: The dataframe containing the H-1B results
   #' @param segment_col: A string containing the name of the column along which outcomes should be calculated 
@@ -15,41 +57,58 @@ bump_plot <- function(df, segment_col, segment_name, outcome_col, outcome_name, 
   #' @param label_mapping: A list mapping the values in a given column to more readable values; also maps
   #' name changes to the same group (for the highest volume groups only)
   
-  most_common_categories <- df %>%
-    mutate(!!as.symbol(segment_col) := recode(!!as.symbol(segment_col), !!!label_mapping)) %>%
-    group_by(!!as.symbol(segment_col)) %>%
-    summarise(num_obs = n()) %>%
-    arrange(desc(num_obs)) %>%
-    top_n(num_categories) %>%
-    pull(!!as.symbol(segment_col))
+  top_n_categories <- identify_top_n_categories(df, segment_col, segment_name, num_categories, label_mapping)
   
-  agg_df <- df %>%
-    mutate(!!as.symbol(segment_col) := recode(!!as.symbol(segment_col), !!!label_mapping)) %>%
-    group_by(lottery_year, !!as.symbol(segment_col)) %>%
-    summarise(outcome_rate = sum(!!as.symbol(outcome_col))/n(), num_obs = n()) %>%
-    filter(!!as.symbol(segment_col) %in% most_common_categories) %>%
+  rank_df <- estimate_outcome_rates(df, segment_col, segment_name, outcome_col, outcome_name, top_n_categories, label_mapping) %>%
     arrange(lottery_year, desc(outcome_rate)) %>%
     mutate(rank = row_number()) %>%
     filter(rank <= num_categories) %>%
     mutate(!!as.symbol(segment_col) := fct_reorder(!!as.symbol(segment_col), rank))
   
-  ggplot(agg_df, aes(x = lottery_year, y = rank, group = !!as.symbol(segment_col), color = !!as.symbol(segment_col))) +
+  ggplot(rank_df, aes(x = lottery_year, y = rank, group = !!as.symbol(segment_col), color = !!as.symbol(segment_col))) +
     geom_line(size = 2) +
     geom_point(size = 6) +
     geom_text(aes(label = rank), vjust = 0.6, size = 3.5, color = 'white') +
     scale_y_reverse(breaks = 1:nrow(df)) +
     scale_color_discrete(name = segment_name)+
-    labs(x = "Lottery Year", y = glue("Rank (Based on {outcome_name})"), title = title, subtitle = glue("Sample is limited to top {num_categories} {str_to_lower(segment_name)} by application volume"), caption = "Visualization: Vinay Tummarakota, @unboxpolitics\nSource: USCIS (Obtained by Bloomberg)") +
+    labs(x = "Lottery Year (FY)", y = glue("Rank (Based on {outcome_name})"), title = title, subtitle = glue("Sample is limited to top {num_categories} {str_to_lower(segment_name)} by application volume"), caption = "Visualization: Vinay Tummarakota, @unboxpolitics\nSource: USCIS (Obtained by Bloomberg)") +
     theme_minimal()
 }
 
-line_plot <- function(df, segment_col, segment_name, outcome_col, outcome_name, min_val, max_val, title) {
-  #' @description: Estimates the rate of {outcome_col} by {segment_col} across lottery years and 
-  #' plots the results using a line plot
+line_plot_categorical <- function(df, segment_col, segment_name, outcome_col, outcome_name, num_categories, label_mapping, title) {
+  #' @description: Estimates the rate of {outcome_col} by {segment_col} across lottery years and plots the results using a line plot
   #' 
   #' @param df: The dataframe containing the H-1B results
   #' @param segment_col: A string containing the name of the column along which outcomes should be calculated 
   #' @param segment_name: A string containing the plain language name of the column along which outcomes should be calculated
+  #' @param outcome_col: A string containing the name of the column containing the outcome of interest
+  #' @param outcome_name: A string containing the plain language name of the column containing the outcome of interest
+  #' @param num_categories: An integer representing the # of categories we wish to include in this visualization (based on the N most frequent categories in the dataset)
+  #' @param label_mapping: A list mapping the values in a given column to more readable values; also maps
+  #' name changes to the same group (for the highest volume groups only)
+  
+  top_n_categories <- identify_top_n_categories(df, segment_col, segment_name, num_categories, label_mapping)
+  
+  rate_df <- estimate_outcome_rates(df, segment_col, segment_name, outcome_col, outcome_name, top_n_categories, label_mapping) %>%
+    mutate(!!as.symbol(segment_col) := fct_reorder(!!as.symbol(segment_col), desc(outcome_rate)))
+  
+  ggplot(rate_df, aes(x = lottery_year, y = outcome_rate, group = !!as.symbol(segment_col), col = !!as.symbol(segment_col)))+
+    geom_line()+
+    geom_point()+
+    labs(x = "Lottery Year (FY)", y = outcome_name, title = title, caption = "Visualization: Vinay Tummarakota, @unboxpolitics\nSource: USCIS (Obtained by Bloomberg)")+
+    scale_y_continuous(labels = percent_format())+
+    scale_color_discrete(name = segment_name)+
+    theme_minimal()
+  
+}
+
+line_plot_continuous <- function(df, segment_col, segment_name, outcome_col, outcome_name, min_val, max_val, title) {
+  #' @description: Estimates the rate of {outcome_col} by {segment_col} across lottery years and 
+  #' plots the results using a line plot
+  #' 
+  #' @param df: The dataframe containing the H-1B results
+  #' @param segment_col: A string containing the name of the continuous-valued column along which outcomes should be calculated 
+  #' @param segment_name: A string containing the plain language name of the continuous-valued column along which outcomes should be calculated
   #' @param outcome_col: A string containing the name of the column containing the outcome of interest
   #' @param outcome_name: A string containing the plain language name of the column containing the outcome of interest
   #' @param min_val: The lowest possible value in {segment_col} that should be plotted
@@ -65,8 +124,8 @@ line_plot <- function(df, segment_col, segment_name, outcome_col, outcome_name, 
   ggplot(agg_df, aes(x = !!as.symbol(segment_col), y = outcome_rate, group = lottery_year, col = lottery_year))+
     geom_line()+
     xlim(min_val, max_val)+
-    labs(x = "Age (Years)", y = glue("{outcome_name} Rate"), title = title, subtitle = glue("Sample is limited to {min_val}-{max_val} year olds"))+
-    scale_y_continuous(labels = scales::percent_format())+
-    scale_color_discrete("Lottery Year")+
+    labs(x = "Age (Years)", y = glue("{outcome_name} Rate"), title = title, subtitle = glue("Sample is limited to {min_val}-{max_val} year olds"), caption = "Visualization: Vinay Tummarakota, @unboxpolitics\nSource: USCIS (Obtained by Bloomberg)")+
+    scale_y_continuous(labels = percent_format())+
+    scale_color_discrete("Lottery Year (FY)")+
     theme_minimal()
 }
